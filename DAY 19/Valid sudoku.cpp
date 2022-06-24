95a553ae39ef756bf2a99bd1c85437a06a407ff5
KAJAL SRIVASTAVA <73694048+kajal1913061@users.noreply.github.com>
2022-06-24 16:51:01 +0530

    Create Valid sudoku.cpp

---
+++ DAY 19/Valid sudoku.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+// tc O(9(n ^ 2)) SC O(1)
+bool isSafe(int row , int col , int matrix[9][9] , int c){
+    for(int i = 0;i<9;i++){
+     if(matrix[i][col]==c){
+         return false;}
+
+ if(matrix[row][i]==c){
+   return false;}
+        
+        if(matrix[3 * (row/3) + i/3][3 * (col/3) +i%3]==c)
+            return false;
+}
+return true;
+}
+bool solve(int matrix[9][9]){
+
+for(int i = 0 ;i<9;i++){
+
+ for(int j = 0 ; j<9;j++){
+
+  if(matrix[i][j]==0){
+
+   for(int k = 1 ;k<=9 ;k++){
+
+    if(isSafe(i,j,matrix,k)){
+
+     matrix[i][j]=k;
+
+     if(solve(matrix)==true)
+
+      return true;
+
+     else
+
+      matrix[i][j]=0;
+
+    }
+
+   }
+
+   return false;
+
+  }
+
+ }
+
+}
+
+return true;
+
+}
+
+ 
+bool isItSudoku(int matrix[9][9]) {
+    return solve(matrix);
+    
+}
